886984bb18eeb4fdecb468d60e4e6a8525a0a768
Adam Ashdown <info@aashdown.co.uk>
2024-08-15 15:30:55 +0100

    steam register hotfix

--- src/components/SteamRegister.tsx
+++ src/components/SteamRegister.tsx
@@ -9,8 +9,13 @@ type RegisterSteam = {
     password: string;
     passwordConfirm: string;
 }
+type RegisterMutate = {
+    username: string;
+    email: string;
+    password: string;
+}
 function SteamRegister() {
-    const [error, setError] = useState<string | null>(null);
+    const [errors, setErrors] = useState<{ [key: string]: string | null }>({});
     const navigate = useNavigate();
     const userJwt = AuthenticatedUser();
     if (userJwt === false) {
@@ -27,14 +32,15 @@ function SteamRegister() {
     // handle the submission of the form via mutation
     
     const registerMutate = useMutation({
-        mutationFn: async (url: string) => {
-            const response = await fetch(url, {
+        mutationFn: async (register: RegisterMutate) => {
+            const response = await fetch(`${import.meta.env.VITE_CLOUD_API_URL}api/Registration/SetUsernameAndPassword`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                     Authorization: `Bearer ${userJwt}`,
                 },
-            })
+                body: JSON.stringify(register),
+            });
 
             if (!response.ok) {
                 const errorResponse = await response.json();
@@ -55,50 +61,56 @@ function SteamRegister() {
         },
         onError: (error: any, response) => {
             console.log(response);
-            setError(`There was an error registering your account. ${JSON.parse(error.message).message}`);
+            setErrors({ form: `There was an error registering your account. ${JSON.parse(error.message).message}` });
         }
     });
-    function doSteamRegister(e: React.FormEvent): void {
+    const doSteamRegister = (e: React.FormEvent): void => {
         e.preventDefault();
-        if (registerData.password != registerData.passwordConfirm) {
-            setError('Passwords do not match');
+        const newErrors: { [key: string]: string | null } = {};
+    
+        if (!registerData.username) {
+            newErrors.username = 'Username is required';
+        } else if (!isValidUsername(registerData.username)) {
+            newErrors.username = 'Username is not valid, please use only letters, numbers, and underscores.';
+        }
+    
+        if (!registerData.email) {
+            newErrors.email = 'Email is required';
+        } else if (!registerData.email.includes('@')) {
+            newErrors.email = 'Email is not valid';
+        }
+    
+        if (!registerData.password) {
+            newErrors.password = 'Password is required';
+        } else if (!isValidPassword(registerData.password)) {
+            newErrors.password = 'Password must be at least 8 characters long and include at least one digit, one lowercase letter, one uppercase letter, and one special character (e.g., !@#$%^&*).';
+        }
+    
+        if (!registerData.passwordConfirm) {
+            newErrors.passwordConfirm = 'Password confirmation is required';
+        } else if (registerData.password !== registerData.passwordConfirm) {
+            newErrors.passwordConfirm = 'Passwords do not match';
+        }
+    
+        if (Object.keys(newErrors).length > 0) {
+            setErrors(newErrors);
             return;
         } else {
-            const queryString: string = `username=${registerData.username}&email=${registerData.email}&password=${registerData.password}`
-            
-            if(error == "") {
-            registerMutate.mutate(`${import.meta.env.VITE_CLOUD_API_URL}api/Registration/SetUsernameAndPassword?${queryString}`);
-            if (registerMutate.isSuccess) {
-                window.location.href = '/account';
-            }
-            if (registerMutate.isError || userError) {
-                setError("There was an error registering your account.");
-            }
-            }
+            registerMutate.mutate({
+                username: registerData.username,
+                email: registerData.email,
+                password: registerData.password
+            });
         }
-    }
+    };
     useEffect(() => {
         if (user != null && user != undefined && !userError) {
             setRegisterData({ ...registerData, username: user.username, email: user.email});
         }
     }, [setRegisterData, user]);
 
-    function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
-        setError(null);
-        // Validate username if it's being updated
-        if (name === "username" && !isValidUsername(value)) {
-            setError('Username is not valid, please use only letters, numbers, and underscores.');
-        }
-        if(name === "email" && !value.includes('@')) {
-            setError('Email is not valid');
-        }
-        if (name === "password" && !isValidPassword(value)) {
-            setError('Password must be at least 8 characters long and include at least one digit, one lowercase letter, one uppercase letter, and one special character (e.g., !@#$%^&*).');
-        }
-        if (name === "passwordConfirm" && value !== registerData.password) {
-            setError('Passwords do not match.');
-        }
         setRegisterData({ ...registerData, [name]: value });
     }
     if (user && !checkNewSteamUserStatus(user)) {
@@ -113,30 +125,30 @@ function SteamRegister() {
                 <h1>Register</h1>
                 <p>You successfully registered with steam</p>
                 <p className="alert alert-success">Complete your account below, this will allow you to login via steam or these credentials</p>
-                {error && <p className="alert alert-info">{error}</p>}
-                <form onSubmit={doSteamRegister} className='w-100'>
-                    <div className="form-group mb-3">
-                        <input onChange={handleInputChange} className="w-100 form-control" type="text" name="username" id="username" title="username" placeholder="Username" value={registerData.username} />
-                    </div>
-                    <div className="form-group mb-3">
-                        <input onChange={handleInputChange} className="w-100 form-control" type="text" name="email" id="email" title="email" placeholder="Email" />
-                    </div>
-                    <div className="form-group mb-3">
-                        <input onChange={handleInputChange} className="w-100 form-control" type="password" name="password" id="password" title="password" placeholder="Password" />
-                    </div>
-                    <div className="form-group mb-3">
-                        <input onChange={handleInputChange} className="w-100 form-control" type="password" name="passwordConfirm" id="passwordConfirm" title="passwordConfirm" placeholder="Confirm Password" />
-                    </div>
-                    <button className="btn login-button w-100" type="submit" disabled={error !== null}>Register</button>
-                </form>
+                {errors.form && <p className="alert alert-info">{errors.form}</p>}
+                <form onSubmit={doSteamRegister}>
+                        <div className="form-group mb-3">
+                            <input onChange={handleInputChange} className={`w-100 form-control ${errors.username ? 'field-error' : ''}`} type="text" name="username" id="username" title="username" placeholder="Username" />
+                            {errors.username && <p className="field-error-message">{errors.username}</p>}
+                        </div>
+                        <div className="form-group mb-3">
+                            <input onChange={handleInputChange} className={`w-100 form-control ${errors.email ? 'field-error' : ''}`} type="text" name="email" id="email" title="email" placeholder="Email" />
+                            {errors.email && <p className="field-error-message">{errors.email}</p>}
+                        </div>
+                        <div className="form-group mb-3">
+                            <input onChange={handleInputChange} className={`w-100 form-control ${errors.password ? 'field-error' : ''}`} type="password" name="password" id="password" title="password" placeholder="Password" />
+                            {errors.password && <p className="field-error-message">{errors.password}</p>}
+                        </div>
+                        <div className="form-group mb-3">
+                            <input onChange={handleInputChange} className={`w-100 form-control ${errors.passwordConfirm ? 'field-error' : ''}`} type="password" name="passwordConfirm" id="passwordConfirm" title="passwordConfirm" placeholder="Confirm Password" />
+                            {errors.passwordConfirm && <p className="field-error-message">{errors.passwordConfirm}</p>}
+                        </div>
+                        <div id="turnstile-widget"></div>
+                        <button className={`btn login-button w-100 ${Object.keys(errors).length > 0 ? "disabled" : ""}`} type="submit">{registerMutate.isPending ? "Registering" : "Register"}</button>
+                    </form>
             </div>
             ) : null
         }
-            {
-                registerMutate.isPending ? (
-                    <Modal type={"Loading"} message={"Creating account"} data={undefined} />
-                ) : null
-            }
         </>
     );
 }
